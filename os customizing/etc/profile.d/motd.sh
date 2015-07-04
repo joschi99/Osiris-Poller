5d3628b317d2efbef71d953fb5225e2d3087822a
Joschi <jochen.platzgummer@bi-s.it>
2015-07-04 22:58:41 +0200

    Standard settings

---
+++ os customizing/etc/profile.d/motd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+echo -e "
+##################################
+#
+# Osiris 2.1.0 NMS Poller
+#
+# All rights reserved
+# (c)2014, BI-S des Jochen Platzgummer
+# info@bi-s.it, support@bi-s.it
+# www.bi-s.it
+# 
+# Welcome to `hostname`
+#
+# This system is running `cat /etc/redhat-release`
+# kernel is `uname -r`
+#
+# You are logged in as `whoami`
+#
+##################################
+"
